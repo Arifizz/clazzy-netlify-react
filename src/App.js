86b6b14fdeb47e878ae25af523d87ec7c0ef79ab
Amir Zakwan <amirzakwanr3@gmail.com>
2021-09-04 10:36:24 +0800

    1. install axios 2. test university api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import logo from './assets/img/My Logo1.png';
+import React, { useState } from 'react';
+import logo from './assets/img/My Logo.png';
+import axios from 'axios';
 
 function App() {
+
+  const [countryName, setCountryName] = useState('')
+
+  const getUniversityList = () => { 
+    axios.get(`http://universities.hipolabs.com/search?country=${countryName}`)
+    .then(response => {
+      console.log('response: ', response.data)
+    }) 
+    .catch(error => {
+      console.log('error: ', error)
+    })
+  }
+
+  const getCountryName = (element) => {
+    console.log('ele: ', element.target.value)
+    const findCountry = element.target.value
+    setCountryName(findCountry)
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +35,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to Amir's World ecehhhhh
+        Find your University
         </a>
+
+        <input type="text" onChange={getCountryName}></input>
+
+        <button onClick={getUniversityList}>
+          Get University List
+        </button>
       </header>
     </div>
   );
